799a4b55b3906ffd0c8548a56b9488c6507143de
villageshop1 <gyanvermalps@gmail.com>
2015-07-08 12:57:03 +0530

    email6

--- sendemail.php
+++ sendemail.php
@@ -4,14 +4,14 @@
 		'type'=>'success',
 		'message'=>'Thank you for contact us. As early as possible  we will contact you '
 	);
-    
+
    $name = @trim(stripslashes($_POST['name'])); 
 $email = @trim(stripslashes($_POST['email'])); 
 $subject = @trim(stripslashes($_POST['subject'])); 
 $message = @trim(stripslashes($_POST['message']));
 
 $email_from = $email;
-$email_to = 'youremail@youremail.com';//replace with your email
+$email_to = 'coolest.vip@gmail.com';//replace with your email
 
 $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
